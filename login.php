6f9c32ac6415e2623c231cd5fafec1c382c768b1
Kenny Stepney <kennystepney@hotmail.com>
2015-04-08 12:58:15 -0500

    added some more php stuff
    mostly cleaning up the code

--- login.php
+++ login.php
@@ -1,5 +1,28 @@
 <?php 
-  echo "Login Successful";
-  $username = $_POST['username'];
-  $id = $_POSt['id'];
+    $username = "";
+    $id = "";
+    $err = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if(empty($_POST["username"])){
+            $err = "No name present";
+        }else{
+          $username = test_input($_POST["username"]);
+        }
+        
+        if(empty($_POST["id"])){
+          $err =  "No id present";
+        }else{
+          $id = test_input($_POST["id"]);
+        }
+    }
+    //removes all charactors that messes up the prorgram
+    function test_input($input){
+        //remove whitespace
+         $input = trim($input);
+         //pretty straight forward, removes slashes
+         $input = stripslashes($input);
+         //removes special cahractors
+         $input = htmlspecialchars($input);
+         return $input;
+    }
 ?>
